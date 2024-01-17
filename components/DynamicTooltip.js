3b1963cf16038470c8f1f235954b998e98ad24a4
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-17 11:15:15 +0530

    Update DynamicTooltip.js

--- components/DynamicTooltip.js
+++ components/DynamicTooltip.js
@@ -2,14 +2,20 @@ import { useState, useEffect } from 'react';
 import TooltipPortal from './TooltipPortal';
 
 function DynamicTooltip() {
+  const [content, setContent] = useState('');
 
-
- 
+  useEffect(() => {
+    // Fetching the content dynamically
+    // fetch('/api/tooltip')
+    setContent("this is a tooltip")
+    //   .then(res => res.json())
+    //   .then(data => setContent(data.content));
+  }, []);
 
   return (
     <TooltipPortal>
       <div style={{ position: 'absolute', top: '50px', left: '50px', backgroundColor: 'gray', padding: '10px', borderRadius: '5px' }}>
-   
+        {content}
       </div>
     </TooltipPortal>
   );
